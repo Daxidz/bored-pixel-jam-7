89fc0243d4fce266c50251ed00346908e2ea8cf9
David Truan <david.truan@heig-vd.ch>
2020-01-20 17:52:18 +0100

    Bouces working, with a way to enable/disable them and fix a max bounces.

--- actors/Mobs/Boss/ProjectileBoss.gd
+++ actors/Mobs/Boss/ProjectileBoss.gd
@@ -4,9 +4,12 @@ const PROJECTILE_SPEED = 200
 var direction
 
 var dmg
-var speed = 200
+var speed = 250
 var velocity = Vector2()
 var projectile_damage = 1
+
+var bounce = false
+var bounces_count = 0
 
 func start(pos, dir):
 	rotation = dir
@@ -26,22 +29,17 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		if collision.collider_shape == null:
+		if collision.collider.name == "TileMap":
+			if bounce and  bounces_count >= 2:
+				velocity = velocity.bounce(collision.normal)
+				bounces_count += 1
+			else:
+				queue_free()
+			
+		if collision.collider.has_method("take_damage"):
+			collision.collider.take_damage(projectile_damage)
 			self.queue_free()
-		else:
-			velocity = velocity.bounce(collision.normal)
-			if collision.collider.has_method("take_damage"):
-				collision.collider.take_damage(projectile_damage)
-				self.queue_free()
 
 func shoot(start_pos, end_pos):
 	self.global_position = start_pos
 	direction = (end_pos - start_pos).normalized()
-
-func _on_Bandage_body_entered(body):
-	if body.get_name() == "Player":
-		body.take_damage(dmg)
-		queue_free()
-		
-	if body.get_name() == "TileMap":
-		queue_free()
