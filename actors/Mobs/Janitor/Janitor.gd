dffe0829d45985d520fe5e24ba8518086d35d7b6
David Truan <david@truan.ch>
2020-01-20 22:59:18 +0100

    Janitor: add animations. Cleanest commit to this day.

--- actors/Mobs/Janitor/Janitor.gd
+++ actors/Mobs/Janitor/Janitor.gd
@@ -24,3 +24,31 @@ func _physics_process(delta):
 			var collision = get_slide_collision(0)
 			if collision.collider.name == "Player":
 				collision.collider.take_damage(2)
+				
+				
+func _process(delta):
+	
+	var a = velocity.angle()
+	var animation = ""
+		
+	var flip_x = false
+
+	
+	if (a > PI / 4) and (a < (PI * 3/4)):
+		animation = "walk_front"
+#	elif (a > ((PI * 5) / 4)) and (a < PI * 7 / 4):
+	elif (a < -PI / 4) and (a > (-PI * 3/4)):
+		animation = "walk_back"
+	else:
+		
+		animation = "walk_side"
+		if velocity.x < 0:
+			flip_x = true
+		else:
+			flip_x = false
+		
+	$Sprite.flip_h = flip_x
+	
+	if animation != $AnimationPlayer.current_animation and animation != "":
+		$AnimationPlayer.play(animation)
+	pass
