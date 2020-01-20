7044c3fd64784f8515187d85a2017a9c7e66e70e
Gallouche <theo@monito.com>
2020-01-20 22:24:21 +0100

    janitor + surgeon

--- actors/Mobs/Janitor/Janitor.gd
+++ actors/Mobs/Janitor/Janitor.gd
@@ -2,9 +2,9 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	show()
+	player = get_node("/root/Main/GameManager/Player")
 
-export var run_speed = 300
+export var run_speed = 150
 const DEFAULT_MASS = 2.0
 var velocity = Vector2.ZERO
 var player = null
@@ -14,26 +14,13 @@ func _physics_process(delta):
 	velocity = Vector2.ZERO
 	var desired_velocity = Vector2.ZERO
 	var steering = Vector2.ZERO
-	var mouse_pos = get_global_mouse_position()
 	
-
 	if player:
 		desired_velocity = (player.position - position).normalized() * run_speed
 		steering = (desired_velocity - velocity) / DEFAULT_MASS
-		#print(velocity)
 		velocity = velocity + steering
 		velocity = move_and_slide(velocity)
 		if get_slide_count() != 0:
 			var collision = get_slide_collision(0)
 			if collision.collider.name == "Player":
-				collision.collider.take_damage(1)
-		#position = position.linear_interpolate(player.position, delta * run_speed)
-
-func _on_DetectRadius_body_entered(body):
-	if body.get_name() == "Player":
-    	player = body
-		
-func _on_DetectRadius_body_exited(body):
-	if body.get_name() == "Player":
-    	player = null
-
+				collision.collider.take_damage(2)
--- actors/Mobs/Surgeon/Surgeon.gd
+++ actors/Mobs/Surgeon/Surgeon.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 func _ready():
 	show()
 
-export var run_speed = 300
+export var run_speed = 340
 const DEFAULT_MASS = 2.0
 var velocity = Vector2.ZERO
 var player = null
@@ -20,12 +20,16 @@ func _physics_process(delta):
 	if player:
 		desired_velocity = (player.position - position).normalized() * run_speed
 		steering = (desired_velocity - velocity) / DEFAULT_MASS
-		#print(velocity)
 		velocity = velocity + steering
 		velocity = move_and_slide(velocity)
-		#position = position.linear_interpolate(player.position, delta * run_speed)
+		if get_slide_count() != 0:
+			var collision = get_slide_collision(0)
+			if collision.collider.name == "Player":
+				collision.collider.take_damage(1)
 
 func _on_DetectRadius_body_entered(body):
 	if body.get_name() == "Player":
-    player = body
+		player = body
 
+func _on_DetectRadius_body_exited(body):
+	player = null # Replace with function body.
